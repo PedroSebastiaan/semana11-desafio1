0320d16e63cd7d95cfb1df8773782b7416563f71
PedroSebastiaan <pizarropedrov@gmail.com>
2021-07-21 13:02:57 -0400

    add: errors component

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
   def create
+    @post = Post.create(title: params[:title], image_url: params[:image_url], content: params[:content])
   end
 end
