b7a1c338bd3541a28d85c256cd7d62b9621c73ab
PedroSebastiaan <pizarropedrov@gmail.com>
2021-07-21 23:08:36 -0400

    fix: url validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
     validates :title, presence: true
-    validates :image_url, presence: true
+    validates :image_url, presence: true, format: URI::regexp(%w[http https])
     validates :content, presence: true
 end
